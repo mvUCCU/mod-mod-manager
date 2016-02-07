652e1d1a9f23c1b3dd44071b4edacc72add492ae
Yangff <Yangff@users.noreply.github.com>
2016-02-07 22:16:20 +0800

    Update index.js
    update to next API V2

--- index.js
+++ index.js
@@ -24,58 +24,61 @@ var readLocalFile = function(name) {
 })
 
 var addMainMenu = function() {
-  var qml = ModAPI.QMLFile("Main/MainMenu.qml")
-  var rootNode = qml.root.node
+  var qml = ModAPI.QMLFileV2("Main/MainMenu.qml")
+  var rootNode = qml.root
 
-  var rootMenus = rootNode.publicMember("menuBar").object.node.objects
+  var rootMenus = rootNode.get("menuBar").obj()
 
   var helpMenu = _.find(rootMenus, function(i) {
-    return i.node.object("title").value == 'qsTr("Help")'
+    return i.get("title") == 'qsTr("Help")'
   }) || _.last(rootMenus)
 
-  var aboutModMenu = helpMenu.node.newObject().createNode()
-  aboutModMenu.describe = "MenuItem"
-  aboutModMenu.object("text", 'qsTr("About") + " mvUCCU..."')
-  aboutModMenu.object("enabled", 'true')
-  aboutModMenu.object("onTriggered", 'root.aboutMod()')
+  var aboutModMenu = ModAPI.QMLCompileV2([
+    "MenuItem {",
+      "text: qsTr(\"About\") + \" mvUCCU...\"",
+      "enabled: true",
+      "onTriggered: root.aboutMod()",
+    "}"
+  ].join("\n"))
 
-  var aboutModSignal = rootNode.publicMember("aboutMod")
-  aboutModSignal.kind = "signal"
-  aboutModSignal.statment = "()"
+  helpMenu.add(aboutModMenu)
+
+  rootNode.def("aboutMod", "Signal", "")
 
   var toolsMenu = _.find(rootMenus, function(i) {
-    return i.node.object("title").value == 'qsTr("Tools")'
+    return i.get("title") == 'qsTr("Tools")'
   }) || rootMenus[5]
 
-  var openModManagerMenu = toolsMenu.node.newObject().createNode()
-  openModManagerMenu.describe = "MenuItem"
-  openModManagerMenu.object("text", '"Mod Manager..."')
-  openModManagerMenu.object("enabled", 'true')
-  openModManagerMenu.object("onTriggered", 'root.openModManager()')
 
-  var openModManagerSignal = rootNode.publicMember("openModManager")
-  openModManagerSignal.kind = "signal"
-  openModManagerSignal.statment = "()"
+  var openModManagerMenu = ModAPI.QMLCompileV2([
+    "MenuItem {",
+      "text: \"Mod Manager...\"",
+      "enabled: true",
+      "onTriggered: root.openModManager()",
+    "}",
+  ].join("\n"))
+
+  toolsMenu.add(openModManagerMenu)
+
+  rootNode.def("openModManager", "Signal", "")
 
   qml.save()
 }
 
 var addMainAction = function() {
-  var qml = ModAPI.QMLFile("Main/MainWindow.qml")
-  var rootNode = qml.root.node
+  var qml = ModAPI.QMLFileV2("Main/MainWindow.qml")
+  var rootNode = qml.root
 
-  var mainMenu = qml.getObjectById("mainMenu").node
-  mainMenu.object("onAboutMod", "aboutModBox.open()")
-  mainMenu.object("onOpenModManager", "modManagerBox.item.open()")
+  var mainMenu = rootNode.getObjectById("mainMenu")
+  mainMenu.set("onAboutMod", "aboutModBox.open()")
+  mainMenu.set("onOpenModManager", "modManagerBox.item.open()")
 
-  var aboutModBox = rootNode.newObject().createNode()
-  aboutModBox.describe = "Dialog_AboutMod"
-  aboutModBox.object("id", "aboutModBox")
+  var aboutModBox = ModAPI.QMLCompileV2("Dialog_AboutMod { id: aboutModBox }")
+  rootNode.add(aboutModBox)
 
-  var modManagerBox = rootNode.newObject().createNode()
-  modManagerBox.describe = "Loader"
-  modManagerBox.object("id", "modManagerBox")
-  modManagerBox.object("source", '"../ModManager/Dialog_ModManager.qml"')
+  var modManagerBox = ModAPI.QMLCompileV2("Loader { id:modManagerBox \n source: \"../ModManager/Dialog_ModManager.qml\"}")
+  rootNode.add(modManagerBox)
+
   qml.save()
 }
 
